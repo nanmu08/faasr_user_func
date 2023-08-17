c904b2dee77739074ea8233d57a658e8f275f75b
Nan Mu <67906227+nanmu08@users.noreply.github.com>
2023-08-17 01:15:51 -0400

    Update simple_sequence1.R

--- simple_sequence1.R
+++ simple_sequence1.R
@@ -1,5 +1,6 @@
 #library("FaaSr")
-        
+library(ggplot2)
+library(dplyr)       
 F1 <- function(faasr) {
   # print("Hello from inside function F1")
   args <- faasr_get_user_function_args(faasr)
@@ -22,6 +23,22 @@ F1 <- function(faasr) {
 F2 <- function(faasr) {
   # print("Hello from inside function Fs")
   args <- faasr_get_user_function_args(faasr)
+
+  # test uninstall package case
+  # Create a sample data frame
+  data <- data.frame(
+  name = c("A", "B", "C", "D"),
+  value = c(3, 8, 6, 10)
+  )
+
+  # Use dplyr to filter rows where value is greater than 5
+  filtered_data <- data %>%
+  filter(value > 5)
+
+  # Plot the filtered data using ggplot2
+  ggplot(filtered_data, aes(x = name, y = value)) +
+  geom_bar(stat = "identity")
+        
   # return arguments for this function (F2)
   # expects input1, input2: input file names; output: 
   faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
