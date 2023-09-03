00e8723dbf2070f55532c9228def13990f9d00e0
Nan Mu <67906227+nanmu08@users.noreply.github.com>
2023-09-03 14:49:35 -0400

    Update compute_product.R

--- compute_product.R
+++ compute_product.R
@@ -8,6 +8,40 @@ compute_product <- function(folder, input1, input2, output) {
   #
   faasr_get_file(faasr, "My_S3_Bucket", folder, input1, "local", "input1.csv")
   faasr_get_file(faasr, "My_S3_Bucket", folder, input2, "local", "input2.csv")
+
+
+ # ---------test uninstalled package-----------
+  is_installed <- function(pkg_name) {
+      return(pkg_name %in% rownames(installed.packages()))
+  }
+  
+  packages_to_test <- c("janitor", "lubridate", "broom", "textclean")
+  
+  for(pkg in packages_to_test){
+    if(is_installed(pkg)){
+      cat(pkg, "is installed\n")
+    } else {
+      cat(pkg, "is NOT installed\n")
+    }
+  }
+     # Create a sample data frame
+  df <- data.frame(
+    "First Name" = c("John", "Jane", "Doe"),
+    "Last-Name" = c("Doe", "Doe", "John"),
+    "Age___" = c(20, 25, 30),
+    "Email.Address" = c("john.doe@email.com", "jane.doe@email.com", "doe.john@email.com")
+  )
+  
+  print("Before cleaning:")
+  print(df)
+  
+  # Use janitor to clean the names
+  clean_df <- clean_names(df)
+  
+  print("After cleaning:")
+  print(clean_df)
+  # ---------end test uninstalled package-----------
+
   
   # This demo function computes output <- input1 * input2 and stores the output back into S3
   # First, read the local inputs, compute the sum, and store the output locally
